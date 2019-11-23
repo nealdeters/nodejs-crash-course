c559b22cb5ef4847aa4aae509734042b3c1bbfc9
Neal Deters <nealdeters@Neals-MBP.attlocal.net>
2019-11-22 18:28:21 -0600

    added procfile and changed port number

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(80, '127.0.0.1', () => {
+server.listen(3000, '127.0.0.1', () => {
   console.log('listening for requests now')
 })
 
