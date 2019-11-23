e09ba34deb1a63af8a38b71b90cb0120aae7217a
Neal Deters <nealdeters@Neals-MBP.attlocal.net>
2019-11-22 18:32:07 -0600

    added port const and passed to the server listener

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const port = process.env.PORT || 3000
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
@@ -50,7 +51,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, '127.0.0.1', () => {
+server.listen(port, '127.0.0.1', () => {
   console.log('listening for requests now')
 })
 
